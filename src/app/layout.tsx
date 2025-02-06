6f5cc993dd2b65d7d38bd3fb37ca57d22de01472
Shaahid <shaahid2453@gmail.com>
2025-02-06 09:27:48 -0800

    seo changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,11 +9,31 @@ const inter = Inter({ subsets: ["latin"] });
 
 import Navbar from "../components/layout/Navbar";
 
-
-export const metadata: Metadata = {
+export const metadata = {
   title: "Kuesuto: AI-powered quizzes on any topic, anytime!",
-  description: "Welcome to Kuesuto, the revolutionary platform that transforms the way you learn and test your knowledge! Imagine having the power to create quizzes on literally anything under the sun, from the depths of ancient history to the latest pop culture phenomena. That's exactly what Kuesuto offers, and so much more!",
+  description:
+    "Welcome to Kuesuto, the revolutionary platform that transforms the way you learn and test your knowledge! Imagine having the power to create quizzes on literally anything under the sun, from the depths of ancient history to the latest pop culture phenomena. That's exactly what Kuesuto offers, and so much more!",
+  openGraph: {
+    type: "website",
+    url: "https://kuesuto.vercel.app/",
+    title: "Kuesuto: AI-powered quizzes on any topic, anytime!",
+    description:
+      "Welcome to Kuesuto, the revolutionary platform that transforms the way you learn and test your knowledge! Imagine having the power to create quizzes on literally anything under the sun, from the depths of ancient history to the latest pop culture phenomena. That's exactly what Kuesuto offers, and so much more!",
+    images: ["/seo.jpg"], 
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "x.com/_kuesuto", 
+    title: "Kuesuto: AI-powered quizzes on any topic, anytime!",
+    description:
+      "Welcome to Kuesuto, the revolutionary platform that transforms the way you learn and test your knowledge! Imagine having the power to create quizzes on literally anything under the sun, from the depths of ancient history to the latest pop culture phenomena. That's exactly what Kuesuto offers, and so much more!",
+    images: ["/seo.jpg"], 
+  },
+  other: {
+    "google-site-verification": "PNPubIbBFBlKdFdiJ2FTQB0tHZjxHE-KecYJ-ZBGpTo",
+  },
 };
+
 
 export default function RootLayout({
   children,
